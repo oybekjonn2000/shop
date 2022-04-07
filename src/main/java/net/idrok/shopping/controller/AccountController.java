a0473553276fa573c4a9b47df26ad83c01fe3d8a
OYBEK <botirov_2000@bk.ru>
2022-04-07 17:15:54 +0500

    фссщгте

--- src/main/java/net/idrok/shopping/controller/AccountController.java
+++ src/main/java/net/idrok/shopping/controller/AccountController.java
@@ -1,4 +1,19 @@
 package net.idrok.shopping.controller;
 
+import net.idrok.shopping.security.UserSpecial;
+import net.idrok.shopping.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class AccountController {
+    @Autowired
+     UserService userService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody UserSpecial userSpecial){
+
+        return "token qaytar";
+    }
+}
 }
